ce28a3c686163dc0d03fe9995102b6944699961e
Irina-Bondarenko <96590953+Irina-Bondarenko@users.noreply.github.com>
2022-10-17 20:45:19 +0300

    init

---
+++ modal/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import ModalApp from "./components/ModalApp";
+
+function App() {
+  return (
+    <div className="App">
+      <ModalApp />
+    </div>
+  );
+}
+
+export default App;
